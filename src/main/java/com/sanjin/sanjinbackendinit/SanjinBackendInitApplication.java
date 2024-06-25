0c37b73bf6102a044d08d001676fef9def9bed20
lisanjin9 <lisanjin9@163.com>
2024-06-25 21:56:29 +0800

    初始化后端项目模板

---
+++ src/main/java/com/sanjin/sanjinbackendinit/SanjinBackendInitApplication.java
@@ -0,0 +1,13 @@
+package com.sanjin.sanjinbackendinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjinBackendInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanjinBackendInitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanjin/sanjinbackendinit/SanjinBackendInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjin.sanjinbackendinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanjinBackendInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
